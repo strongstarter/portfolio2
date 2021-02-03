17cfb81f9af88e5a3560c6e1cb5472bd0de052cd
strongstarter <43767525+strongstarter@users.noreply.github.com>
2021-02-03 21:43:26 +0900

    JS: navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ document.addEventListener('scroll', () => {
     }
 })
 
+
 // Handle scrolling when tapping on the navbar menu.
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -29,11 +30,21 @@ navbarMenu.addEventListener('click', (event) => {
         if (link == null) {
          return;
         }
+
+    
     // console.log(event.target.dataset.link); //이 부분 이해 안감.  
     // const scrollTo = document.querySelector(link); // link라는 element를 받아와서 
     // scrollTo.scrollIntoView({behavior: "smooth"}); //scrollTO에 scrollIntoView
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+// Navbar toggle button for smaller screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open'); // navbarMenu에 클래스에 리스트를 토글 해주도록 하겠음. 토글할클래스는 open.
+});
+
 
 //  Scrollting to Contact when clicking the "Contact Me" button.
 const ContactMeBtn = document.querySelector('.home__contact');
